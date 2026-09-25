836920788321fe5af61bf72568cc57a2721cd085
FabianToral-vegamedia <fabian.toral.lozano@ces-vegamedia.es>
2026-09-25 10:09:50 +0200

    Add files via upload

---
+++ MisOperaciones.java
@@ -0,0 +1,22 @@
+package ejemplos;
+
+public class MisOperaciones {
+
+	public static void main(String[] args) {
+		// Declaración e inicialización de variables
+		int x=3, y=8;
+		
+		System.out.println("x="+x+" y="+y);
+		
+		// Operaciones
+		x++;
+		y--;
+		System.out.println("x="+x+" y="+y);
+		x+=y;
+		System.out.println("x="+x+" y="+y);
+		x-=3;
+		y*=2;
+		System.out.println("x="+x+" y="+y);
+
+	}
+}
