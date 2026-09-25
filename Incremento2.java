94483c60ce07d579edefda19b739a37722fd4c43
FabianToral-vegamedia <fabian.toral.lozano@ces-vegamedia.es>
2026-09-25 09:58:30 +0200

    Add files via upload

---
+++ Incremento2.java
@@ -0,0 +1,18 @@
+package ejemplos;
+
+public class Incremento2 {
+
+	public static void main(String[] args) {
+		int a=5;					// Declaro e inicializo la variable a
+		System.out.println(a);		// Muestro el valor de a
+		a++;						// Incremento el valor de a
+		System.out.println(a);		// Muestro el valor de a
+		++a;						// Incremento el valor de a
+		System.out.println(a);		// Muestro el valor de a
+		System.out.println(a++);	// Muestro el valor de a y después lo inremento 
+		System.out.println(a);		// Muestro el valor de a
+		System.out.println(++a);	// Inremento el valor de a y después lo muestro 
+
+	}
+
+}
